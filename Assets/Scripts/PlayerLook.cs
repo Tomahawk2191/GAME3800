5f634d0052adb4050209dc14ffc62ee3a4a5d915
noahwc <noahwcheng@gmail.com>
2026-01-25 20:15:54 -0500

    Camera Control
    Implemented playerlook script for camera control

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform PlayerCamera;
+    public Vector2 MouseSensitivity;
+
+    private Vector2 rotationXY;
+
     void Start()
     {
         
@@ -11,6 +15,19 @@ public class PlayerLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!PlayerCamera)
+        {
+            return;
+        }
+
+        Vector2 MouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        rotationXY.x -= MouseInput.y * MouseSensitivity.y;
+        rotationXY.y += MouseInput.x * MouseSensitivity.x;
+
+        rotationXY.x = Mathf.Clamp(rotationXY.x, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(0f, rotationXY.y, 0f);
+        PlayerCamera.localEulerAngles = new Vector3(rotationXY.x, 0f, 0f);
     }
 }
